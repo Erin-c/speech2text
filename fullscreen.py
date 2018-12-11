346edf60d8ad67e187409fd3dac1ff4567316e13
l00sed <dwt46@cornell.edu>
2018-12-11 20:21:35 +0000

    python3 compatible fullscreen.py

--- fullscreen.py
+++ fullscreen.py
@@ -4,19 +4,24 @@ import subprocess
 import time
 import sys
 
+dict_lookup=['wearable', 'picture', 'book']
+
 import os, shutil
 if os.path.isdir("downloads"):
     shutil.rmtree('downloads')
 
-def fullscreen(keyword):
-    response = google_images_download.googleimagesdownload()
-    arguments = {"keywords":keyword, "limit":1, "format":"jpg", "size":"large", "aspect_ratio":"tall", "print_urls":True}
-    image_path = response.download(arguments)
-    try:
-        print(image_path[keyword][0])
-        sp = subprocess.Popen(["geeqie", "-f", image_path[keyword][0]])
-        time.sleep(3)
-        #Erin, you can change this to complete some other action before closing the window. You can also just keep the window open by removing the line below:
-        sp.terminate()
-    except IndexError:
-        print "No images found for that keyword query."
+def fullscreen(keywords):
+    sep_keyword = keywords.split()
+    for keyword in sep_keyword:
+        if keyword in dict_lookup:
+            response = google_images_download.googleimagesdownload()
+            arguments = {"keywords":keyword, "limit":1, "format":"jpg", "size":"large", "aspect_ratio":"tall", "print_urls":True}
+            image_path = response.download(arguments)
+            try:
+                print(image_path[keyword][0])
+                sp = subprocess.Popen(["geeqie", "-f", image_path[keyword][0]])
+                #time.sleep(3)
+                #Erin, you can change this to complete some other action before closing the window. You can also just keep the window open by removing the line below:
+                sp.terminate()
+            except IndexError:
+                print("No images found for that keyword query.")
